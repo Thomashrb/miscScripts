32b42e0d08ba65231fcdb397cadeff18541d1bed
Thomashrb <thomas_laammas@hotmail.com>
2017-11-14 19:15:57 +0100

    changed to NC-theme rofisurfraw

--- rofisurfraw.sh
+++ rofisurfraw.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Depends on rofi | surfraw | firefox
-surfraw -browser=firefox $(sr -elvi | awk -F'-' '{print $1}' | sed '/:/d' | awk '{$1=$1};1' | rofi -kb-row-select "Tab" -kb-row-tab "Control+space" -color-window "#000000, #000000, #000000" -color-normal "#000000, #b3e774, #000000, #b3e774, #000000" -color-active "#000000, #b3e774, #000000, #b3e774, #000000" -color-urgent "#000000, #b3e774, #000000, #b3e774, #000000" -dmenu -mesg ">>> Tab = Autocomplete" -i -p "websearch: ")
+surfraw -browser=firefox $(sr -elvi | awk -F'-' '{print $1}' | sed '/:/d' | awk '{$1=$1};1' | rofi -kb-row-select "Tab" -kb-row-tab "Control+space" -color-window "#3F8AC2, #3F8AC2, #3F8AC2" -color-normal "#3F8AC2, #ffffff, #3F8AC2, #ffffff, #3F8AC2" -color-active "#3F8AC2, #ffffff, #3F8AC2, #ffffff, #000000" -color-urgent "#3F8AC2, #000000, #3F8AC2, #000000, #3F8AC2" -dmenu -mesg ">>> Tab = Autocomplete" -i -p "websearch: ")
