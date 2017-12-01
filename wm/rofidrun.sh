04860167d05d0fdfad40b5ec0e8d8a418a06be88
Thomashrb <thomas_laammas@hotmail.com>
2017-12-01 12:58:00 +0100

    drun script, replaces dmenu

---
+++ wm/rofidrun.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rofi -combi-modi window,drun -show combi -color-window '#3F8AC2, #3F8AC2, #3F8AC2' -color-normal '#3F8AC2, #ffffff, #3F8AC2, #ffffff, #3F8AC2' -color-active '#3F8AC2, #ffffff, #3F8AC2, #ffffff, #000000' -color-urgent '#3F8AC2, #000000, #3F8AC2, #000000, #3F8AC2'
