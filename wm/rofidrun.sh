6ba9a97285b1ed44d55ec559a389c6a61550d231
Thomashrb <thomas_laammas@hotmail.com>
2017-12-01 13:00:40 +0100

    add comment

--- wm/rofidrun.sh
+++ wm/rofidrun.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+#depends on rofi
 rofi -combi-modi window,drun -show combi -color-window '#3F8AC2, #3F8AC2, #3F8AC2' -color-normal '#3F8AC2, #ffffff, #3F8AC2, #ffffff, #3F8AC2' -color-active '#3F8AC2, #ffffff, #3F8AC2, #ffffff, #000000' -color-urgent '#3F8AC2, #000000, #3F8AC2, #000000, #3F8AC2'
